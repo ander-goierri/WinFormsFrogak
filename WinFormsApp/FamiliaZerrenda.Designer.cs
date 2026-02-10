3b07c3246f8cd27e7df630f1ef4fa3ebd30749a7
Ander <amadinabeitia.goierri@gmail.com>
2026-02-10 16:56:05 +0100

    Familia zerrenda gehituta, nahiz eta DenakLortu metodoa ez dagoen ongi

---
+++ WinFormsApp/FamiliaZerrenda.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WinFormsApp
+{
+    partial class FamiliaZerrenda
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        private void InitializeComponent()
+        {
+            mainTableLayout = new TableLayoutPanel();
+            titleBody = new FlowLayoutPanel();
+            titleBodyLabel = new Label();
+            gridContainerPanel = new Panel();
+            familiaGridView = new DataGridView();
+            mainTableLayout.SuspendLayout();
+            titleBody.SuspendLayout();
+            gridContainerPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)familiaGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // mainTableLayout
+            // 
+            mainTableLayout.BackColor = Color.Silver;
+            mainTableLayout.ColumnCount = 1;
+            mainTableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            mainTableLayout.Controls.Add(titleBody, 0, 0);
+            mainTableLayout.Controls.Add(gridContainerPanel, 0, 1);
+            mainTableLayout.Dock = DockStyle.Fill;
+            mainTableLayout.Location = new Point(0, 0);
+            mainTableLayout.Name = "mainTableLayout";
+            mainTableLayout.RowCount = 2;
+            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 12F));
+            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 88F));
+            mainTableLayout.Size = new Size(1009, 772);
+            mainTableLayout.TabIndex = 0;
+            // 
+            // titleBody
+            // 
+            titleBody.BackColor = Color.Transparent;
+            titleBody.Controls.Add(titleBodyLabel);
+            titleBody.Dock = DockStyle.Fill;
+            titleBody.Location = new Point(3, 3);
+            titleBody.Name = "titleBody";
+            titleBody.Size = new Size(1003, 86);
+            titleBody.TabIndex = 0;
+            // 
+            // titleBodyLabel
+            // 
+            titleBodyLabel.BackColor = Color.IndianRed;
+            titleBodyLabel.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            titleBodyLabel.ForeColor = Color.Black;
+            titleBodyLabel.Location = new Point(3, 0);
+            titleBodyLabel.Name = "titleBodyLabel";
+            titleBodyLabel.Size = new Size(1000, 60);
+            titleBodyLabel.TabIndex = 0;
+            titleBodyLabel.Text = "Familien zerrenda";
+            titleBodyLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // gridContainerPanel
+            // 
+            gridContainerPanel.BackColor = Color.Transparent;
+            gridContainerPanel.Controls.Add(familiaGridView);
+            gridContainerPanel.Dock = DockStyle.Fill;
+            gridContainerPanel.Location = new Point(3, 95);
+            gridContainerPanel.Name = "gridContainerPanel";
+            gridContainerPanel.Size = new Size(1003, 674);
+            gridContainerPanel.TabIndex = 1;
+            // 
+            // familiaGridView
+            // 
+            familiaGridView.AllowUserToAddRows = false;
+            familiaGridView.AllowUserToDeleteRows = false;
+            familiaGridView.AllowUserToResizeRows = false;
+            familiaGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            familiaGridView.Location = new Point(20, 20);
+            familiaGridView.MultiSelect = false;
+            familiaGridView.Name = "familiaGridView";
+            familiaGridView.ReadOnly = true;
+            familiaGridView.RowHeadersVisible = false;
+            familiaGridView.RowTemplate.Height = 29;
+            familiaGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            familiaGridView.Size = new Size(960, 620);
+            familiaGridView.TabIndex = 0;
+            // 
+            // FamiliaZerrenda
+            // 
+            Controls.Add(mainTableLayout);
+            Name = "FamiliaZerrenda";
+            Size = new Size(1009, 772);
+            mainTableLayout.ResumeLayout(false);
+            titleBody.ResumeLayout(false);
+            gridContainerPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)familiaGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel mainTableLayout;
+        private FlowLayoutPanel titleBody;
+        private Label titleBodyLabel;
+        private Panel gridContainerPanel;
+        private DataGridView familiaGridView;
+    }
+}
---
+++ WinFormsApp/FamiliaZerrenda.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using WinFormsApp.Controllerak;
+
+namespace WinFormsApp
+{
+    public partial class FamiliaZerrenda : UserControl
+    {
+        public FamiliaZerrenda()
+        {
+            InitializeComponent();
+
+            this.Resize += new EventHandler(MainForm_Resize);
+            ConfigureGrid();
+            LoadDemoData();
+            //SetFamiliaDataFromApi();
+            UpdatePanelSizes();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            UpdatePanelSizes();
+        }
+
+        public void UpdatePanelSizes()
+        {
+            UpdateLayoutSizes();
+        }
+
+        private void UpdateLayoutSizes()
+        {
+            int fullWidth = this.ClientSize.Width;
+
+            // Título ocupa todo el ancho
+            titleBody.Width = fullWidth;
+            titleBodyLabel.Width = fullWidth;
+
+            // Grid centrado y ocupando casi todo
+            int padding = 20;
+            familiaGridView.Width = gridContainerPanel.Width - (padding * 2);
+            familiaGridView.Height = gridContainerPanel.Height - (padding * 2);
+            familiaGridView.Location = new Point(padding, padding);
+        }
+
+        private void ConfigureGrid()
+        {
+            familiaGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            familiaGridView.AutoGenerateColumns = true;
+            familiaGridView.BackgroundColor = Color.Gainsboro;
+            familiaGridView.BorderStyle = BorderStyle.None;
+        }
+
+        private void LoadDemoData()
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("Id", typeof(int));
+            dt.Columns.Add("Izena", typeof(string));
+            dt.Columns.Add("Helbidea", typeof(string));
+            dt.Columns.Add("Telefonoa", typeof(string));
+
+            dt.Rows.Add(1, "Madinabeitia", "Ordizia", "600123456");
+            dt.Rows.Add(2, "Etxeberria", "Tolosa", "600987654");
+            dt.Rows.Add(3, "Aranburu", "Legorreta", "600555444");
+
+            familiaGridView.DataSource = dt;
+        }
+
+        public void SetFamiliaDataFromApi()
+        {
+            familiaGridView.DataSource = FamiliakController.DenakLortu();
+        }
+    }
+}
